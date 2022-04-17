c572987837e95444a4e4bba4d7927b720a0968ad
Franco Cabrera <64378629+FrancoCabrera25@users.noreply.github.com>
2022-04-16 21:25:56 -0300

    change theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,31 @@
 import "../styles/globals.css";
 import type { AppContext, AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { darkTheme, customTheme } from "../themes";
-import Cookies from 'js-cookie';
+import { darkTheme, customTheme, lightTheme } from "../themes";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 
-function MyApp({ Component, pageProps, ...rest }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const [currentTheme, setCurrentTheme] = useState(lightTheme);
 
+  useEffect(() => {
+    const cookieTheme = Cookies.get("theme") || "light";
+    const selectedTheme =
+      cookieTheme === "light"
+        ? lightTheme
+        : cookieTheme === "dark"
+        ? darkTheme
+        : customTheme;
 
-
+    setCurrentTheme(selectedTheme);
+  }, []);
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   );
-}
-
-MyApp.getInitialProps =async (appContext: AppContext) => {
-  
-  const { theme } = appContext.ctx.req ? ( appContext.ctx.req as any).cookies :  'light' 
-
-  return {
-    theme
-  }
 }
 
 export default MyApp;
